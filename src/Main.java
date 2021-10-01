39607798f738bb68a4a1a79558e7971b92a53145
4exan <chekhanovskyi@gmail.com>
2021-10-01 17:33:09 +0300

    Final test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+    static void reverse(int[] array){
+        for(int i = array.length-1; i != -1; i--){
+            System.out.println(array[i] + " ");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] array = {1, 2 ,3 ,4, 5};
+        reverse(array);
     }
 }
